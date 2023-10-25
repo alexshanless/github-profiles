a342e9445f51c698cf7dd6a8f6122096cf69136f
Alex Bilba <alex.shanless@gmail.com>
2023-10-25 08:59:15 -0700

    react setup

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='text-2xl font-light'>
+        Created by{' '}
+        <a href='https://alexbilba.com' className='text-blue-600'>
+          {' '}
+          Alex
+        </a>{' '}
+      </p>
+    </div>
+  );
+}
+
+export default About;
