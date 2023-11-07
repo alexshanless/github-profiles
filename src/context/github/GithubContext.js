f6b83ab8d4b482ad97730d2b4034e2aa866a6c07
Alex Bilba <alex.shanless@gmail.com>
2023-11-06 18:07:58 -0800

    first reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,21 +1,30 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducers';
 
 const GithubContext = createContext();
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const intialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, intialState);
 
   const getUsers = async () => {
     const response = await fetch(`https://api.github.com/users`);
 
     const data = await response.json();
-    setUsers(data);
-    setLoading(false);
+    dispatch({
+      type: 'GET_USERS',
+      payload: data,
+    });
   };
 
   return (
-    <GithubContext.Provider value={{ users, loading, getUsers }}>
+    <GithubContext.Provider
+      value={{ users: state.users, loading: state.loading, getUsers }}
+    >
       {children}
     </GithubContext.Provider>
   );
