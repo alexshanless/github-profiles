7c12c65bef3d3e7ad702d743fc19cf347403fff3
Alex Bilba <alex.shanless@gmail.com>
2024-03-06 09:35:24 -0800

    update

--- src/components/users/RepoList.jsx
+++ src/components/users/RepoList.jsx
@@ -4,7 +4,7 @@ function RepoList({ repos }) {
   return (
     <div className='reounded-lg shadow-lg card bg-base-100'>
       <div className='card-body'>
-        <h2 className='text-3xl my-4 font-bold card-title'>Top Repos</h2>
+        <h2 className='text-3xl my-4 font-bold card-title'>Latest Repos</h2>
         {repos.map(repo => (
           <RepoItem key={repo.id} repo={repo} />
         ))}
