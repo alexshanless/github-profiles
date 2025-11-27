0db798e09a78efd6564c47f1af5fc7c77f13b436
Alex Bilba <alex.shanless@gmail.com>
2025-11-27 10:32:11 -0800

    new version

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import UserSearch from './components/userSearch';
+
+const App = () => {
+	return (
+		<div className='container'>
+			<h1>Github Finder</h1>
+			<UserSearch />
+		</div>
+	);
+};
+
+export default App;
